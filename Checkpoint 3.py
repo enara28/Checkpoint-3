754a96cd90137073127e104478e80a9fadf68ef8
enara28 <enara.zeama@gmail.com>
2025-04-15 12:06:11 +0200

    Add files via upload

---
+++ Checkpoint 3.py
@@ -0,0 +1,38 @@
+#M2C3 Python Assignment
+
+#Actividad 1
+hilo = "Hola, mundo"
+numero = 28
+lista = ["uno", "dos", "tres", "cuatro"]
+booleano = True
+
+#Actividad 2
+corte = hilo[0:3]
+
+#Actividad 3
+elemento_lista = lista[0]
+
+#Actividad 4
+numero_dos = numero + 10
+
+#Actividad 5
+ultimo_lista = lista[3]
+
+#Actividad 6
+names = 'harry,alex,susie,jared,gail,conner'
+names_list = names.split(",")
+
+#Actividad 7
+hilo_mayus = hilo[0:4].upper()
+hilo_total = hilo_mayus + hilo[4:]
+
+#Actividad 8
+saludo = f"Buenos días, Enara, vemos que ya no tienes {numero} años."
+
+#Actividad 9
+print('hello world')
+
+#Actividad extra
+bienvenida = "Hola, María"
+consulta = bienvenida.find("Hola")
+despedida = bienvenida.replace("Hola", "Adiós")
